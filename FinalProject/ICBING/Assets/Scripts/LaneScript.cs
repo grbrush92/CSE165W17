01ad1f4dcef39bdec6c78fc67670472e36eb088f
grbrush92 <mcatozzi@UCSD.EDU>
2017-03-21 10:50:40 -0700

    Finished Product
    100% done

---
+++ FinalProject/ICBING/Assets/Scripts/LaneScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneScript : MonoBehaviour {
+
+    public HandRadial radial;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("0");
+
+        if (other.gameObject.tag == "Lane1")
+        {
+            Debug.Log("1");
+            radial.setLane1();
+        }
+        if (other.gameObject.tag == "Lane2")
+        {
+            Debug.Log("2");
+            radial.setLane2();
+        }
+        if (other.gameObject.tag == "Lane3")
+        {
+            Debug.Log("3");
+            radial.setLane3();
+        }
+    }
+}
